05fb7027fc148ecefce796c7a5d5ec2e12d3f2a9
KANAKTANDON <knktndn@gmail.com>
2024-10-14 00:26:05 +0530

    testng

--- src/test/java/project1/zomato.java
+++ src/test/java/project1/zomato.java
@@ -1,76 +1,73 @@
 package project1;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test; // Import the TestNG annotation
 
 public class zomato {
 
-	static WebDriver driver;
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kanak Tandon\\NewEclipseWorkspace\\chromedriver_129.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		
-		driver.get("https://www.forbes.com/billionaires/");
-		Thread.sleep(20000);		
-		
-		//scroll to richest people in world header
-		scrollDownToXpath("//h2[normalize-space()='The Richest People In The World']");
-		Thread.sleep(5000);
-		
-		
-		//click on country
-		driver.findElement(By.xpath("(//span[normalize-space()='country / Territory'])[1]")).click();
-		//scroll to india element from dropdown
-		scrollDownToXpath("//li[@value='India']");
-		//click on india
-		driver.findElement(By.xpath("//li[@value='India']")).click();
-		Thread.sleep(10000);
-		//find and store all 250 movie in list 
-		List<WebElement> india_richest_people = driver.findElements(By.xpath("//div[contains (@class,'Table_personName')]"));
-		
-		
-		System.out.println("number of india_richest_people in forbes: "+ india_richest_people.size());
-		System.out.println();
-		for (WebElement e: india_richest_people) {
-			
-			if (!e.getText().trim().equals("NAME")) {
-				System.out.println(e.getText().trim());
-				
-			}
-			
-		}
-		
-		
-		
+    static WebDriver driver;
 
-}
-	
-	public static void scrollDownToXpath(String xpath) throws Exception {
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		WebElement e = driver.findElement(By.xpath(xpath));
+    @Test // Annotate the method with @Test
+    public void forbes() throws Exception {
+        // Setup ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kanak Tandon\\NewEclipseWorkspace\\chromedriver_129.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
-		int counter = 1;
-		while (counter < 5) {
-			// if element found , exit the loop
-			if (e.isDisplayed()) {
-//				System.out.println("element found");
-				break;
-				// if not found , continue the loop
-			} else {
-				System.out.println("element not found");
-				js.executeScript("window.scrollBy(0,50)");
-				Thread.sleep(5000);
-				counter++;
-			}
-		}
-	}
+        // Navigate to Forbes Billionaires List
+        driver.get("https://www.forbes.com/billionaires/");
+        Thread.sleep(20000);
+
+        // Scroll to richest people in world header
+        scrollDownToXpath("//h2[normalize-space()='The Richest People In The World']");
+        Thread.sleep(5000);
+
+        // Click on country
+        driver.findElement(By.xpath("(//span[normalize-space()='country / Territory'])[1]")).click();
+        // Scroll to India element from dropdown
+        scrollDownToXpath("//li[@value='India']");
+        // Click on India
+        driver.findElement(By.xpath("//li[@value='India']")).click();
+        Thread.sleep(10000);
+        
+        // Find and store all Indian richest people in a list
+        List<WebElement> india_richest_people = driver.findElements(By.xpath("//div[contains (@class,'Table_personName')]"));
+
+        // Output the number of richest people
+        System.out.println("Number of Indian richest people in Forbes: " + india_richest_people.size());
+        System.out.println();
+        
+        // Print the names of the richest people
+        for (WebElement e : india_richest_people) {
+            if (!e.getText().trim().equals("NAME")) {
+                System.out.println(e.getText().trim());
+            }
+        }
+
+        // Close the driver
+        driver.quit(); // It's good practice to close the driver after test execution
+    }
+
+    public static void scrollDownToXpath(String xpath) throws Exception {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement e = driver.findElement(By.xpath(xpath));
+
+        int counter = 1;
+        while (counter < 5) {
+            // If the element is found, exit the loop
+            if (e.isDisplayed()) {
+                break;
+            } else {
+                System.out.println("Element not found");
+                js.executeScript("window.scrollBy(0,50)");
+                Thread.sleep(5000);
+                counter++;
+            }
+        }
+    }
 }
